73df14c39242c5c5530ec70966b2a2ff77311275
Martin Morrison <martomorr@gmail.com>
2023-07-13 14:41:53 -0300

    Ver mas producto

--- src/pages/Producto.js
+++ src/pages/Producto.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
-export default function Producto(props) {
-    return(
-        <div>
-            <h1>{props.producto}</h1>
-            <p>{props.descripcion}</p>
-            <b>Precio: </b><p>U$D{props.precio}</p>
-            <b>Rating: </b><p>{props.precio}⭐</p>
-            <img src={props.imagen} />
-        </div>
-    )
+export default function Producto() {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+
+  console.log(id);
+
+  let url = "https://dummyjson.com/products/" + id;
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      console.log(res);
+      setProduct(res.data);
+    });
+  }, [url]);
+
+  console.log(product);
+
+  return (
+    <div>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <p><b>Precio: </b>U$D{product.price}</p>
+      <p><b>Rating: </b>{product.rating}⭐</p>
+      <img src={product.thumbnail} alt="" />
+    </div>
+  );
 }
