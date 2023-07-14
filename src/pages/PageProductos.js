477c0392ed6e436f149ceeace54120f3d5928363
Martin Morrison <martomorr@gmail.com>
2023-07-14 12:36:54 -0300

    carousel productos funca (gracias chatgpt)

--- src/pages/PageProductos.js
+++ src/pages/PageProductos.js
@@ -1,44 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PageProductos.css";
-import { useEffect, useState } from "react";
 import CardProducto from "../components/CardProducto";
 import ListaCategorias from "../components/ListaCategorias";
 import axios from "axios";
-import RowCards from "../components/RowCards";
 
 export default function PageProductos() {
   const [productos, setProductos] = useState([]);
   const [categories, setCategories] = useState([]);
-  let skip = 0;
-  const limit = 30;
-  let url = "https://dummyjson.com/products?limit=" + limit + "&skip=" + skip;
+  const [skip, setSkip] = useState(0);
+  const limit = 100;
+  const url = `https://dummyjson.com/products?limit=${limit}&skip=${skip}`;
 
   useEffect(() => {
-    url = "https://dummyjson.com/products?limit=" + limit + "&skip=" + skip;
     axios.get(url).then((res) => {
       console.log(res.data.products);
-      setProductos(
-        res.data.products.map((p) => ({
+      setProductos((prevProductos) => [
+        ...prevProductos,
+        ...res.data.products.map((p) => ({
           id: p.id,
           title: p.title,
           description: p.description,
           price: p.price,
           rating: p.rating,
           image: p.thumbnail,
-        }))
-      );
+        })),
+      ]);
     });
-    url = "https://dummyjson.com/products/categories";
-    axios.get(url).then((res) => {
+
+    axios.get("https://dummyjson.com/products/categories").then((res) => {
       console.log(res);
       setCategories(res.data);
     });
-  }, []);
+  }, [skip, url]);
 
   const filterProducts = (e) => {
     let text = e.target.value;
-    url = "https://dummyjson.com/products/search?q=" + text;
-    axios.get(url).then((res) => {
+    const searchUrl = `https://dummyjson.com/products/search?q=${text}&limit=${limit}`;
+    axios.get(searchUrl).then((res) => {
       console.log(res);
       setProductos(
         res.data.products.map((p) => ({
@@ -50,7 +48,28 @@ export default function PageProductos() {
           image: p.thumbnail,
         }))
       );
+      setSkip(0);
     });
+  };
+
+  const chunkArray = (arr, size) => {
+    const chunkedArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+  };
+
+  const paginatedProductos = chunkArray(productos, 4);
+
+  const handleNext = () => {
+    setSkip((prevSkip) => prevSkip + 4);
+  };
+
+  const handlePrev = () => {
+    if (skip >= 4) {
+      setSkip((prevSkip) => prevSkip - 4);
+    }
   };
 
   return (
@@ -73,43 +92,65 @@ export default function PageProductos() {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Filtrar por categoria
+                Filtrar por categoría
               </button>
               <ul className="dropdown-menu" id="categories">
                 {categories.map((c) => (
-                  <ListaCategorias categoria={c} />
+                  <ListaCategorias categoria={c} key={c.id} />
                 ))}
               </ul>
             </div>
           </div>
         </div>
       </form>
-      <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-            <RowCards productos={productos} />
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          {paginatedProductos.map((chunk, index) => (
+            <div
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+              key={index}
+            >
+              <div className="row">
+                {chunk.map((p) => (
+                  <CardProducto
+                    id={p.id}
+                    title={p.title}
+                    price={p.price}
+                    rating={p.rating}
+                    image={p.image}
+                    description={p.description}
+                    key={p.id}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
           data-bs-target="#carouselExample"
           data-bs-slide="prev"
+          onClick={handlePrev}
+          style={{ display: skip === 0 ? "none" : "block" }}
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
           data-bs-target="#carouselExample"
           data-bs-slide="next"
+          onClick={handleNext}
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
       {/* <div id="paginacion">
-      <a id="masProductos">Ver mas productos</a>
-    </div> */}
+        <a id="masProductos">Ver más productos</a>
+      </div> */}
     </>
   );
 }
